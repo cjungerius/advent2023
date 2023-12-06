4a54096b4891c52f35531128e7539ea260cba740
Chris Jungerius <cjungerius@gmail.com>
2023-12-06 09:00:00 +0100

    add 'mathy' solution

--- solutions/06.jl
+++ solutions/06.jl
@@ -1,4 +1,5 @@
 function race(time, distance)
+    # How i solved it first: works just fine (~19ms for solve2)
     wins = 0
     peaked = false
     for t in time:-1:1
@@ -13,6 +14,17 @@ function race(time, distance)
     wins
 end
 
+function race_poly(time,distance)
+    # find all solutions where x(t-x) > d:
+    # in other words, where -x^2+tx-d > 0 or x^2 -tx + d > 0
+    # concave, so find both intersects with 0 using discriminant rule
+    discr = time^2-4*distance # b^2 - 4ac
+    sol1 = (time + sqrt(discr))/2
+    sol2 = (time - sqrt(discr))/2
+    return ceil(sol1) - floor(sol2) - 1 # all solutions BETWEEN the two intersects
+end
+
+
 function solve1(io)
 
     input = readlines(io)
@@ -21,7 +33,7 @@ function solve1(io)
 
     total = ones(Int,length(times))
     for i in eachindex(times)
-        total[i] = race(times[i],distances[i])
+        total[i] = race_poly(times[i],distances[i])
     end
     *(total...)
 end
@@ -31,5 +43,5 @@ function solve2(io)
     time = parse(Int,*(split(input[1])[2:end]...))
     distance = parse(Int,*(split(input[2])[2:end]...))
    
-    race(time,distance)
+    race_poly(time,distance)
 end
