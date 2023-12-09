773378290f87fdc50a78458a993086ff5be6153e
Chris Jungerius <cjungerius@gmail.com>
2023-12-09 09:12:21 +0100

    solve day 9

---
+++ solutions/09.jl
@@ -0,0 +1,24 @@
+function solve(io)
+
+    nextvals = Int[]
+    prevvals = Int[]
+
+    for line in eachline(io)
+        obs = parse.(Int,split(line))
+        
+        starts = Int[obs[1]]
+        ends = Int[obs[end]]
+
+        while !all(y->y==obs[1],obs)            
+            obs = diff(obs)
+            push!(starts,obs[1])
+            push!(ends,obs[end])
+        end
+
+        push!(prevvals,foldr(-,starts))
+        push!(nextvals, sum(ends))
+    end
+    sum(nextvals), sum(prevvals)
+end
+
+partone, parttwo = solve("data/09in.txt")
