c9f9abadd4e1f4937978c03f3448154392b43e52
Chris Jungerius <cjungerius@gmail.com>
2023-12-13 01:40:08 +0100

    clean up memo and remove string concat from loop

--- solutions/12.jl
+++ solutions/12.jl
@@ -1,50 +1,49 @@
 
-function solvefast(io,parttwo=false)
+function solvefast(io, parttwo=false)
 
-    memo = Dict([])
+    memo = Dict()
 
-    function testgroups(str, groups::Vector{Int}, g_idx=0, counter=0)
+    function testgroups(str, groups::Vector{Int}, j=1, g_idx=0, counter=0, last=' ')
 
-    if haskey(memo, (str,counter,g_idx)) 
-        return memo[(str,counter,g_idx)]
-    end
+        return get!(memo, (j, counter, g_idx, last)) do
+            for i in j:length(str)
+                x = str[i]
+                if x == '#' || last == '#'
+                    last = ' '
+                    counter == 0 && (g_idx += 1)
+                    # if longer than current group: no match
+                    g_idx > length(groups) && return 0
+                    counter += 1
+                    # if unaccounted for by groups: no match
+                    counter > groups[g_idx] && return 0
+                elseif x == '.' || last == '.'
+                    last = ' '
+                    # if cuts short current group: no match
+                    0 < counter < groups[g_idx] && return 0
+                    counter = 0
+                elseif x == '?'
+                    if g_idx > 0 && 0 < counter < groups[g_idx]
+                        counter += 1
+                    elseif g_idx > 0 && counter == groups[g_idx]
+                        counter = 0
+                    else
+                        a = testgroups(str, groups, i, g_idx, counter, '#')
+                        b = testgroups(str, groups, i, g_idx, counter, '.')
+                        return a + b
+                    end
+                end
+            end
 
-    for (i, x) in enumerate(str)
-        if x == '#'
-            counter == 0 && (g_idx += 1)
-            # if longer than current group: no match
-            g_idx > length(groups) && return 0
-            counter += 1
-            # if unaccounted for by groups: no match
-            counter > groups[g_idx] && return 0
-        elseif x == '.'
-            # if cuts short current group: no match
-            0 < counter < groups[g_idx] && return 0
-            counter = 0
-        elseif x == '?'
-            if g_idx > 0 && 0 < counter < groups[g_idx]
-                counter += 1
-            elseif g_idx > 0 && counter == groups[g_idx]
-                counter = 0
+            if counter == 0 && g_idx == length(groups) || counter == groups[end] && g_idx == length(groups)
+                return 1
             else
-                a = testgroups("#" * str[i+1:end], groups, g_idx, counter)
-                memo[("#" * str[i+1:end], counter, g_idx)] = a
-                b = testgroups("." * str[i+1:end], groups, g_idx, counter)
-                memo[("." * str[i+1:end], counter, g_idx)] = b
-                return a+b
+                return 0
             end
         end
     end
 
-    if counter == 0 && g_idx == length(groups) || counter == groups[end] && g_idx == length(groups)
-        return 1
-    else
-        return 0
-    end
-end
-
     countsum = 0
-    for (i,line) in enumerate(eachline(io))
+    for (i, line) in enumerate(eachline(io))
         str, nums = split(line, " ")
 
         nums = parse.(Int, split(nums, ","))
@@ -52,7 +51,7 @@ end
             str = (str * "?")^4 * str
             nums = repeat(nums, 5)
         end
-        memo = Dict([])
+        empty!(memo)
         countsum += testgroups(str, nums)
     end
 
